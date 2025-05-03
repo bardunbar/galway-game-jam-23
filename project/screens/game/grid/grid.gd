f2517f37224a5981ce747475ec039441d7f3b361
toadkarter <vrakhmanin1@gmail.com>
2025-05-03 12:46:26 +0100

    Adds initial grid to game.

--- project/screens/game/grid/grid.gd
+++ project/screens/game/grid/grid.gd
@@ -3,14 +3,15 @@ extends Node2D
 
 @export var tile_class: PackedScene
 
-@export var grid_width: int
-@export var grid_height: int
+@export var grid_width: int = 8
+@export var grid_height: int = 8
+@export var tile_size: float = 64.0
 
 func _ready() -> void:
 	for i in range(grid_width):
 		for j in range(grid_height):
 			var tile: Tile = tile_class.instantiate() as Tile
 			tile.position = position
-			tile.position.x += (64 * j)
-			tile.position.y += (64 * i)
+			tile.position.x += (tile_size * j)
+			tile.position.y += (tile_size * i)
 			add_child(tile)
